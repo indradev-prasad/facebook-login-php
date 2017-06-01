7e471e4b65c2e700a3f580cd2b3521cc0dab6464
indradev prasad <kumarindradevd9211@gmail.com>
2017-06-01 11:21:36 +0530

    Update fblogin.php

--- fblogin.php
+++ fblogin.php
@@ -9,7 +9,7 @@ $helper = $fb->getRedirectLoginHelper();
 
 $permissions = ['email']; // Optional permissions
 //$permissions = ['state']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('http://localhost/testliabrary/sociallogin/fbcallback.php', $permissions);
+$loginUrl = $helper->getLoginUrl('http://localhost/testliabrary/sociallogin/facebook/fbcallback.php', $permissions);
 echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 //print_r($_SESSION);
 ?>
